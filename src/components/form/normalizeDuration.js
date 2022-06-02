96dd55d83f5a14d74f385f526cb5a162b1b990ed
jzolotar <zolotarjakub@gmail.com>
2022-06-02 19:34:56 +0200

    add small fix to normalize function

--- src/components/form/normalizeDuration.js
+++ src/components/form/normalizeDuration.js
@@ -5,7 +5,7 @@ const normalizeDuration = (value, previousValue) => {
       return onlyNums + ':';
     }
     if (onlyNums.length === 4) {
-      return onlyNums.slice(0, 3) + ':' + onlyNums.slice(3) + '-';
+      return onlyNums.slice(0, 2) + ':' + onlyNums.slice(2) + '-';
     }
   }
   if (onlyNums.length <= 2) {
