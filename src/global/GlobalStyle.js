50f302bd446efce3d4685ff8bd4abcd0a7b46722
jzolotar <zolotarjakub@gmail.com>
2022-06-04 16:28:34 +0200

    add reset styles to override default browser autocomplete styling

--- src/global/GlobalStyle.js
+++ src/global/GlobalStyle.js
@@ -54,6 +54,20 @@ button {
  cursor: pointer;
 }
 
+input:-webkit-autofill,
+input:-webkit-autofill:hover, 
+input:-webkit-autofill:focus,
+textarea:-webkit-autofill,
+textarea:-webkit-autofill:hover,
+textarea:-webkit-autofill:focus,
+select:-webkit-autofill,
+select:-webkit-autofill:hover,
+select:-webkit-autofill:focus {
+  -webkit-text-fill-color: #f4f4f4f4;
+  -webkit-box-shadow: 0 0 0px 1000px #333 inset;
+  transition: background-color 5000s ease-in-out 0s;
+}
+
 `;
 
 export default GlobalStyle;
